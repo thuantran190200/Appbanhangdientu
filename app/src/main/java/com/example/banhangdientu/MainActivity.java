424ee60d707956f2d40eb2e5bc5d849aab32f4ae
Thuận Trần <thuantran190200@gmail.com>
2021-05-29 22:40:09 +0700

    demo

--- app/src/main/java/com/example/banhangdientu/MainActivity.java
+++ app/src/main/java/com/example/banhangdientu/MainActivity.java
@@ -4,24 +4,43 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button next;
+    Animation topAnim, bottomAnim;
+    ImageView image;
+    TextView logo;
+    private static int SPLASH_SCREEN = 4000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        next = findViewById(R.id.next);
 
-        next.setOnClickListener(new View.OnClickListener() {
+        //Animations
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        image = findViewById(R.id.imageview);
+        logo = findViewById(R.id.text1);
+
+        image.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View v) {
-                Intent intent1 = new Intent( MainActivity.this, Giaodienchinh.class);
-                startActivity(intent1);
+            public void run() {
+                Intent intent = new Intent( MainActivity.this, Giaodienchinh.class);
+                startActivity(intent);
+                finish();
             }
-        });
+        }, SPLASH_SCREEN);
+
     }
 }
